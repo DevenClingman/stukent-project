8f133feb99ac074dc1ee0408b886687c0fc60190
Deven Clingman <dclingy@gmail.com>
2018-01-06 19:27:30 -0700

    step 6: added private method in post.rb file to select random number between 1 and 10 and find factorial of number, also created set_defaults method to set value of factorial in table, also edited show page to show factorial selected

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,26 @@
 class Post < ApplicationRecord
+
+  validates_presence_of :title, :body, :factorial
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.factorial = set_factorial
+  end
+
+  private
+
+  def set_factorial
+    total = 0
+    num = 1 + rand(10)
+    if num == 1
+      total = 1
+      return total
+    end
+    total = num * num.pred
+    while num.pred > 1
+      total = total * (num.pred - 1)
+      num -= 1
+    end
+    total
+  end
 end
